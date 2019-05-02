2a040c0fbc3f60773f6ef8b99ae6be60160cac75
BSDinis <baltasardinis@hotmail.com>
2019-05-02 23:19:07 +0100

    did graph, with input

---
+++ graph.cpp
@@ -0,0 +1,41 @@
+#include "graph.hpp"
+
+graph create_graph(std::istream &in)
+{
+  using std::vector;
+
+  int f, e, t;
+  if (!(std::cin >> f)) throw "failed to get f";
+  else if (f <= 0) throw "non positive f";
+  if (!(std::cin >> e)) throw "failed to get e";
+  else if (e <= 0) throw "non positive e";
+  if (!(std::cin >> t)) throw "failed to get t";
+  else if (t <= 0) throw "non positive t";
+
+  graph res(f + e + 2);
+  for (ssize_t i = 0; i < f; i++) {
+    int production_value;
+    if (!(std::cin >> production_value)) throw "failed to get a producer value";
+    res.add_edge(0, i + 2, production_value); // connect dummy source to producer vertex with the capacity of the production
+  }
+
+  for (ssize_t i = 0; i < e; i++) {
+    int max_cap;
+    if (!(std::cin >> max_cap)) throw "failed to get a capacity value";
+    res.change_capacity(i + f + 2, max_cap);
+  }
+
+  for (ssize_t i = 0; i < t; i++) {
+    int o, d, c;
+    if (!(std::cin >> o)) throw "failed to get source val";
+    if (o < 2) throw "invalid source value";
+    if (!(std::cin >> d)) throw "failed to get destination val";
+    if (d < 1) throw "invalid destination value";
+    if (!(std::cin >> c)) throw "failed to get capacity val";
+    if (c < 1) throw "invalid capacity value";
+    res.add_edge(o, d, c);
+  }
+
+  return res;
+}
+
---
+++ graph.hpp
@@ -0,0 +1,87 @@
+#pragma once
+
+#include <vector>
+#include <iostream>
+#include <utility>
+
+#define GRAPH_DEBUG 1
+
+// DUMMY
+#define source (0);
+// TARGET
+#define target (1);
+
+using std::pair;
+using std::make_pair;
+using std::vector;
+class graph {
+
+  struct node {
+    using neighbour = pair<int,int>;
+    int excess   = 0;
+    int cap      = 0;
+    vector<neighbour> neighbours;
+
+    void add(int v, int w) noexcept {
+      neighbours.emplace_back(v, w);
+    }
+  };
+
+  vector<node> node_list;
+  public:
+    graph()          noexcept : graph(128) {} // reserve
+    graph(int n)     noexcept : graph(static_cast<size_t>(n)) { } // reserve
+    graph(ssize_t n) noexcept : graph(static_cast<size_t>(n)) {} // reserve
+    graph(size_t n)  noexcept : node_list(n) { // reserv
+      while (n-- > 0) node_list.emplace_back();
+    }
+    ~graph() = default;
+
+    inline bool has_link(const int u, const int v) const noexcept
+    {
+      if (!(u < V() && v < V())) return false;
+      if (u == v) return true;
+      for (const auto neighbour : node_list[u].neighbours) {
+        if (neighbour.first == v) return true;
+      }
+      return false;
+    }
+
+
+    bool add_edge(const int u, const int v, const int w) noexcept
+    {
+      if (has_link(u, v)) return false;
+      node_list[u].add(v, w);
+      return true;
+    }
+
+    inline int V() const noexcept { return node_list.size(); }
+
+    inline void change_capacity(int u, int w) noexcept { node_list[u].cap = w; }
+
+#if GRAPH_DEBUG
+    std::ostream & print(std::ostream & os) const noexcept {
+      os << "src\t|dst\t|capsrc\t|edgcap\t|\n";
+      const ssize_t sz = node_list.size();
+      for (int i = 0; i < sz; i++) {
+        for (const auto & dst : node_list[i].neighbours) {
+          os << i << "\t|"
+            << dst.first << "\t|"
+            << node_list[i].cap << "\t|"
+            << dst.second << "\t|\n";
+        }
+      }
+
+      return os;
+    }
+#endif
+};
+
+graph create_graph(std::istream &in) ;
+
+#if GRAPH_DEBUG
+inline std::ostream & operator<<(std::ostream &os, const graph & g)
+{
+  return g.print(os);
+}
+#endif
